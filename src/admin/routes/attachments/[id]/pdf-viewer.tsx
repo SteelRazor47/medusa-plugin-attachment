4c08e75c0e59f84add8846945c769203c4335606
SteelRazor47 <36779933+SteelRazor47@users.noreply.github.com>
2025-04-13 15:30:18 +0200

    fix: send full order object and items table to pdf viewer

--- src/admin/routes/attachments/[id]/pdf-viewer.tsx
+++ src/admin/routes/attachments/[id]/pdf-viewer.tsx
@@ -5,6 +5,57 @@ import { image, builtInPlugins, table, multiVariableText } from '@pdfme/schemas'
 import { sdk } from '../../../lib/sdk';
 import { useQuery } from '@tanstack/react-query';
 
+const DEFAULT_PROPERTIES = [
+    "id",
+    "status",
+    "created_at",
+    "canceled_at",
+    "email",
+    "display_id",
+    "currency_code",
+    "metadata",
+    // --- TOTALS ---
+    "total",
+    "item_total",
+    "shipping_subtotal",
+    "subtotal",
+    "discount_total",
+    "discount_subtotal",
+    "shipping_total",
+    "shipping_tax_total",
+    "tax_total",
+    "refundable_total",
+    "order_change",
+]
+
+const DEFAULT_RELATIONS = [
+    "*customer",
+    "*items", // -> we get LineItem here with added `quantity` and `detail` which is actually an OrderItem (which is a parent object to LineItem in the DB)
+    "*items.variant",
+    "*items.variant.product",
+    "*items.variant.options",
+    "+items.variant.manage_inventory",
+    "*items.variant.inventory_items.inventory",
+    "+items.variant.inventory_items.required_quantity",
+    "+summary",
+    "*shipping_address",
+    "*billing_address",
+    "*sales_channel",
+    "*promotion",
+    "*shipping_methods",
+    "*fulfillments",
+    "+fulfillments.shipping_option.service_zone.fulfillment_set.type",
+    "*fulfillments.items",
+    "*fulfillments.labels",
+    "*fulfillments.labels",
+    "*payment_collections",
+    "*payment_collections.payments",
+    "*payment_collections.payments.refunds",
+    "*payment_collections.payments.refunds.refund_reason",
+    "region.automatic_taxes",
+]
+
+const DEFAULT_FIELDS = `${DEFAULT_PROPERTIES.join(",")},${DEFAULT_RELATIONS.join(",")}`
 
 const PDFViewer = ({ template }: { template: Template }) => {
     const viewerRef = useRef(null);
@@ -23,8 +74,11 @@ const PDFViewer = ({ template }: { template: Template }) => {
         }
     }, [template]);
 
-    const { data: order } = useQuery({
-        queryFn: async () => (await sdk.admin.order.list({ fields: "+total,+email,*shipping_address" })).orders[0],
+    const { data: { order } = {} } = useQuery({
+        queryFn: async () => {
+            const id = await sdk.admin.order.list().then((res) => res.orders[0].id)
+            return await sdk.admin.order.retrieve(id, { fields: DEFAULT_FIELDS })
+        },
         queryKey: ["pdfme-order"]
     })
 
@@ -32,11 +86,15 @@ const PDFViewer = ({ template }: { template: Template }) => {
         // Only create the designer if it doesn't already exist
         if (viewerRef.current && !viewerInstanceRef.current) {
             // Initialize the Designer
+            const items = order?.items.map((item) => [item.title, "" + item.total] as const) ?? []
+            items.push(["Shipping", "" + order?.shipping_total])
+            items.push(["Total", "" + order?.total])
+
             viewerInstanceRef.current = new Viewer({
                 domContainer: viewerRef.current,
                 template: template,
                 plugins,
-                inputs: [{ order }]
+                inputs: [{ order, table: items }]
             });
 
             // Clean up on unmount
